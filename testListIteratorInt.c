9dfa6229e9fd5c8641e83389f7ebb89b16abc8e2
davidccs <ddchen30@gmail.com>
2017-04-11 16:38:50 +1000

    Add files via upload

---
+++ testListIteratorInt.c
@@ -0,0 +1,146 @@
+/*
+  client to test listIteratorInt.
+  Written by ....
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "listIteratorInt.h"
+
+
+int main(int argc, char *argv[]){
+  //IteratorInt newList = IteratorIntNew();
+/*  printf("No Nodes\n");
+  printf("====Before Result========\n");
+  print(newList);
+  delete(newList);
+  printf("====After Result=========\n");
+  print(newList);
+  printf("========PASSED===========\n");
+  printf("\n");
+  printf("One Node\n");
+  printf("====Before Result========\n");
+  add(newList,30);
+  previous(newList);
+  print(newList);
+  delete(newList);
+  printf("====After Result=========\n");
+  print(newList);
+  printf("========PASSED===========\n");
+*/
+/*
+  printf("\n");
+  printf("Deleting Front Node\n");
+  printf("====Before Result========\n");
+  add(newList,30);
+  add(newList,40);
+  add(newList,20);
+  previous(newList);
+  previous(newList);
+  previous(newList);
+  print(newList);
+  delete(newList);
+  printf("====After Result=========\n");
+  print(newList);
+  printf("========PASSED===========\n");
+  */
+/*
+  printf("\n");
+  printf("Deleting End Node\n");
+  printf("====Before Result========\n");
+  add(newList,30);
+  add(newList,40);
+  add(newList,20);
+  previous(newList);
+  next(newList);
+  print(newList);
+  delete(newList);
+  printf("====After Result=========\n");
+  print(newList);
+  printf("========PASSED===========\n");
+*/
+  /*printf("\n");
+  printf("Deleting Inbetween Node\n");
+  printf("====Before Result========\n");
+  add(newList,30);
+  add(newList,40);
+  add(newList,20);
+  previous(newList);
+  previous(newList);
+  print(newList);
+  delete(newList);
+  printf("====After Result=========\n");
+  print(newList);
+  printf("========PASSED===========\n");
+*/
+/*
+  add(newList, 30);
+  add(newList, 40);
+  add(newList, 35);
+  add(newList, 24);
+  previous(newList);
+  previous(newList);
+  previous(newList);
+  previous(newList);
+  printPointer(newList);
+  print(newList);
+  findNext(newList, 25);
+  */
+  //printPointer(newList);
+  //previous(newList);
+  //delete(newList);
+  //assert(delete(newList) == 1);
+  /*printPointer(newList);
+  print(newList);
+  reset(newList);
+  printPointer(newList);
+*/
+  //printPointer(newList);
+  /*next(newList);
+  next(newList);
+  next(newList);
+  previous(newList);
+  delete(newList);
+  print(newList);
+  delete(newList);
+  add(newList,5);
+  previous(newList);
+  previous(newList);
+  set(newList, 69);
+  print(newList);
+  delete(newList);
+  print(newList);
+  */
+  /*delete(newList);
+  printPointer(newList);
+  //set(newList,20);
+  printPointer(newList);
+  print(newList);
+*/
+    /*add(newList, 20);
+    add(newList, 12);
+    add(newList, 33);
+    add(newList, 25);
+    print(newList);
+    previous(newList);
+    previous(newList);
+    printf("lemao\n");
+    next(newList);
+    delete(newList);
+    printf("hello\n");
+    previous(newList);
+    printf("OK\n");
+    print(newList);
+    printf("\n");
+    printPointer(newList);
+*/
+
+
+
+  printf("ALL TEST PASSED YOU ARE AWESOME!\n");
+
+  return EXIT_SUCCESS;
+
+}
